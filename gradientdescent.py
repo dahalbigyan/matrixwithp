1fd707f7d0146e8c817ce39c5cabb08e845d540a
bigyan.dahal@knovva.com <bigyan.dahal@knovva.com>
2018-04-08 20:17:55 -0400

    pre function creation.

--- gradientdescent.py
+++ gradientdescent.py
@@ -11,3 +11,30 @@ h(x) = m0 + m1x
 """
 cost_function(j(m)) = 1/m(∑ from i to m)((hm)(x^i) - y^i)^2
 """
+
+from numpy import loadtxt, zeros, ones, array, linspace, logspace
+from pylab import scatter, show, title, xlabel, ylabel, plot, contour
+
+# load the data set
+data = loadtxt('data1.txt', delimiter=',')
+#plot the data
+# scatter(data[:, 0], data[:, 1], marker='o', c='b')
+# title('Profits distribution')
+# xlabel('Population of City in 10,000s')
+# ylabel('Profit in $10,000s')
+# show()
+
+
+x = data[:, 0]
+y = data[:, 1]
+# number of training size
+m = y.size
+# add a column of ones to X(interception data)
+it = ones(shape=(m,2))
+it[:,1] = x
+
+# Initialize theta parameters
+theta = zeros(shape=(2,1))
+predictions = it.dot(theta)
+print(predictions.flatten())
+print(y)
